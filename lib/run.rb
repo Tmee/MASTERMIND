2bb91cc55f7f24a782e970434456fc550014f76d
Timothy Mee <tjmee90@gmail.com>
2014-09-24 14:04:37 -0600

    fixed win and lose output text

--- lib/run.rb
+++ lib/run.rb
@@ -18,42 +18,27 @@ class Run
   end
 
   def play
+    clear_screen
     printer.show_opening
     until win? || exit? || lose?
       printer.guess_request
       @command = gets.strip.split('')
-      # if @command = ["q"] || ["q","u","i","t"]
-      #   exit
-      # else @command.length != 4
-      #   printer.command_length_error
-      # end
-        process_color_guess_elements
-        process_color_guess_position
-        printer.show_correct_positions(correct_positions)
-        printer.show_correct_elements(correct_elements)
-        printer.show_guess_count(guess_count)
-        printer.show_target(target_color_pattern)
-      #end
+      if @command.length != 4
+        printer.command_length_error
+      end
+      process_color_guess_elements
+      process_color_guess_position
+      printer.show_correct_positions(correct_positions)
+      printer.show_correct_elements(correct_elements)
+      printer.show_guess_count(guess_count)
+      printer.show_target(target_color_pattern)
     end
   end
 
   def process_color_guess_elements
     @guess_count += 1
     @correct_elements = 0
-
-    if @target_color_pattern.include?(@command[0])
-      @correct_elements += 1
-    end
-    if @target_color_pattern.include?(@command[1])
-      @correct_elements += 1
-    end
-    if @target_color_pattern.include?(@command[2])
-      @correct_elements += 1
-    end
-    if @target_color_pattern.include?(@command[3])
-      @correct_elements += 1
-    end
-    # printer.show_correct_elements(correct_elements)
+    @command.each { |color| @correct_elements += 1 if target_color_pattern.include?(color) }
   end
 
   def process_color_guess_position
@@ -79,12 +64,19 @@ class Run
 
   def lose?
     if guess_count == 10
-     puts "HAHAHAHA YOU LOST!!!!!!!"
-
+      puts "HAHAHAHA YOU LOST!!!!!!!"
     end
+    guess_count == 10
   end
 
   def win?
+    if command[0..3] == target_color_pattern[0..3]
+      puts "GOOD FUCKING JOB MAN!!!!, YOU WON!!!!!!!"
+    end
     command[0..3] == target_color_pattern[0..3]
+  end
+
+  def clear_screen
+    print "\e[2J\e[f"
   end
 end
